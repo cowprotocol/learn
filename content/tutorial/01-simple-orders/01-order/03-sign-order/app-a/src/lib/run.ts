2c007bd60068cc3de5a4a8889c214c14d8acb224
Jefferson Bastos <jeff.junior98@gmail.com>
2025-08-07 15:25:47 -0300

    feat: refactor sign-order tutorial

--- content/tutorial/01-simple-orders/01-order/03-sign-order/app-a/src/lib/run.ts
+++ content/tutorial/01-simple-orders/01-order/03-sign-order/app-a/src/lib/run.ts
@@ -1,31 +1,46 @@
-import type { Web3Provider } from '@ethersproject/providers'
-import { OrderBookApi, SupportedChainId, OrderQuoteRequest, OrderQuoteSideKindSell } from '@cowprotocol/cow-sdk'
+import type { Web3Provider } from '@ethersproject/providers';
+import {
+	OrderBookApi,
+	SupportedChainId,
+	OrderQuoteRequest,
+	OrderQuoteSideKindSell,
+	setGlobalAdapter
+} from '@cowprotocol/cow-sdk';
+import { EthersV5Adapter } from '@cowprotocol/sdk-ethers-v5-adapter';
+
+// Helper function to setup adapter (will be used in all tutorials from now on)
+function setupAdapter(provider: Web3Provider) {
+	const signer = provider.getSigner();
+	const adapter = new EthersV5Adapter({ provider, signer });
+	setGlobalAdapter(adapter);
+	return { signer, adapter };
+}
 
 export async function run(provider: Web3Provider): Promise<unknown> {
-    const chainId = +(await provider.send('eth_chainId', []));
-    if (chainId !== SupportedChainId.GNOSIS_CHAIN) {
-        throw new Error(`Please connect to the Gnosis chain. ChainId: ${chainId}`);
-    }
+	const chainId = +(await provider.send('eth_chainId', []));
+	if (chainId !== SupportedChainId.GNOSIS_CHAIN) {
+		throw new Error(`Please connect to the Gnosis chain. ChainId: ${chainId}`);
+	}
 
-    const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.GNOSIS_CHAIN });
+	const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.GNOSIS_CHAIN });
 
-    const signer = provider.getSigner();
-    const ownerAddress = await signer.getAddress();
+	const { signer } = setupAdapter(provider);
+	const ownerAddress = await signer.getAddress();
 
-    const sellToken = '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'; // wxDAI
-    const buyToken = '0x177127622c4A00F3d409B75571e12cB3c8973d3c'; // COW
-    const sellAmount = '1000000000000000000'; // 1 wxDAI
+	const sellToken = '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'; // wxDAI
+	const buyToken = '0x177127622c4A00F3d409B75571e12cB3c8973d3c'; // COW
+	const sellAmount = '1000000000000000000'; // 1 wxDAI
 
-    const quoteRequest: OrderQuoteRequest = {
-        sellToken,
-        buyToken,
-        from: ownerAddress,
-        receiver: ownerAddress,
-        sellAmountBeforeFee: sellAmount,
-        kind: OrderQuoteSideKindSell.SELL,
-    };
+	const quoteRequest: OrderQuoteRequest = {
+		sellToken,
+		buyToken,
+		from: ownerAddress,
+		receiver: ownerAddress,
+		sellAmountBeforeFee: sellAmount,
+		kind: OrderQuoteSideKindSell.SELL
+	};
 
-    const { quote } = await orderBookApi.getQuote(quoteRequest);
+	const { quote } = await orderBookApi.getQuote(quoteRequest);
 
-    return quote
+	return quote;
 }

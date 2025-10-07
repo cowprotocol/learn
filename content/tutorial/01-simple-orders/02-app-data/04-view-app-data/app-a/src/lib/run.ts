da23921ccdab3e148f8a03c30c839950e2fb4115
shoom3301 <shoom3301@gmail.com>
2025-10-07 17:49:00 +0500

    fix: remove app-data because it's covered in SDK

--- content/tutorial/01-simple-orders/02-app-data/04-view-app-data/app-a/src/lib/run.ts
+++
@@ -1,9 +0,0 @@
-import type { PublicClient, WalletClient } from 'viem';
-import { OrderBookApi, SupportedChainId } from '@cowprotocol/cow-sdk';
-
-export async function run(publicClient: PublicClient, walletClient: WalletClient): Promise<unknown> {
-	const orderBookApi = new OrderBookApi({ chainId: SupportedChainId.GNOSIS_CHAIN });
-
-	// TODO: Retrieve app data using getAppData method
-	return {};
-}
